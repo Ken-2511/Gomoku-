4540f7ecc898db3982eab181656b5fd66ecfca67
Ken-2511 <IWMAIN@outlook.com>
2023-12-30 16:12:44 -0500

    Update

--- wuziqi.py
+++ wuziqi.py
@@ -21,7 +21,10 @@ class Table:
     
     def occupy(self, x, y):
         """Occupy a place in the table. `b_or_w` is used to specify whether this chess is black or white."""
+        if self.table[x, y] != 0:
+            return
         self.table[x, y] = self.next_chess
+        self.steps.append([x, y])
         self.next_chess = -self.next_chess  # change the black/white
     
     def _in_table(self, x, y):
@@ -64,6 +67,8 @@ class Table:
         return False
     
     def reverse(self):
+        if len(self.steps) == 0:
+            return
         x, y = self.steps.pop(-1)
         self.table[x, y] = 0
         self.next_chess = -self.next_chess
@@ -76,12 +81,11 @@ class Drawer:
         # table
         self.table = table
         # the coordinates of the chess
-        self.grid = np.zeros_like(self.table.table, dtype=np.uint32)
-        for x in range(self.length):
-            for y in range(self.length):
+        self.grid = np.zeros((*self.table.table.shape, 2), dtype=np.uint32)
+        for x in range(length):
+            for y in range(length):
                 self.grid[x, y] = table2grid(x, y)
         self.flatten_grid = self.grid.reshape(-1, 2)
-        print(self.flatten_grid)
     
     def draw_grid(self):
         for i in range(factor, (length + 1) * factor, factor):
@@ -92,7 +96,7 @@ class Drawer:
     def draw_chess(self):
         for x in range(length):
             for y in range(length):
-                color = table[x, y]
+                color = self.table.table[x, y]
                 if color == 0:
                     continue
                 elif color == 1:
@@ -104,20 +108,23 @@ class Drawer:
     
     def get_closest_point(self, mouse_pos):
         point = np.argmin(np.sum(np.abs(self.flatten_grid - mouse_pos), axis=1))
-        y, x = point // length, point % length
+        x, y = point // length, point % length
         return x, y
+    
+    def draw_mouse(self, mouse_pos):
+        pygame.draw.circle(self.screen, (127, 127, 127), mouse_pos, chess_radius // 3)
     
     def draw_temp_chess(self, mouse_pos):
         """draw the `temporary chess` that the mouse hovered on, for better visual experience"""
         x, y = self.get_closest_point(mouse_pos)
-        x, y = table2grid(x, y)
+        pos = table2grid(x, y)
         if win_status == True:
             color = (255, 192, 192)
         elif self.table.next_chess == 1:
             color = (64, 64, 64)
         else:
             color = (192, 192, 192)
-        pygame.draw.circle(self.screen, color, (x, y), chess_radius)
+        pygame.draw.circle(self.screen, color, pos, chess_radius, 4)
 
 
 class Controller:
@@ -126,34 +133,58 @@ class Controller:
         self.drawer = drawer
         self.screen = screen
         self.clock = pygame.time.Clock()
+        pygame.mouse.set_visible(False)
         self.mouse_pos = pygame.mouse.get_pos()
     
     def occupy(self):
         x, y = self.drawer.get_closest_point(self.mouse_pos)
         self.table.occupy(x, y)
+        win_status = table.check_if_win(*self.drawer.get_closest_point(self.mouse_pos))
+        if win_status:
+            print("game over")
+    
+    def move(self, dx, dy):
+        x, y = self.drawer.get_closest_point(self.mouse_pos)
+        x += dx
+        y += dy
+        pos = table2grid(x, y)
+        pygame.mouse.set_pos(pos)
+        self.mouse_pos = pos
 
     def main_loop(self):
         """The game mainly occurs in this fucntion"""
         while True:
+            need_draw = False
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.display.quit()
-                    sys.exit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    self.occupy()
-                    win_status = table.check_if_win(*self.drawer.get_closest_point())
-                    if win_status:
-                        print("game over")
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        self.table.reverse()
+                need_draw = True
                 if event.type == pygame.MOUSEMOTION:
                     self.mouse_pos = pygame.mouse.get_pos()
-            self.drawer.draw_grid()
-            self.drawer.draw_chess()
-            self.drawer.draw_temp_chess(self.mouse_pos)
-            pygame.display.flip()
-            self.clock.tick(60)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.occupy()
+                elif event.type == pygame.QUIT:
+                    pygame.display.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        self.move(0, -1)
+                    elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        self.move(0, 1)
+                    elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.move(-1, 0)
+                    elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.move(1, 0)
+                    elif event.key == pygame.K_RETURN:
+                        self.occupy()
+                    elif event.key == pygame.K_BACKSPACE:
+                        self.table.reverse()
+            if need_draw:
+                self.screen.fill((224, 176, 92))
+                self.drawer.draw_grid()
+                self.drawer.draw_chess()
+                self.drawer.draw_temp_chess(self.mouse_pos)
+                self.drawer.draw_mouse(self.mouse_pos)
+                pygame.display.flip()
+            self.clock.tick(240)
 
 
 if __name__ == '__main__':
